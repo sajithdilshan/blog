7b76a83749abfc5324ec07f13128dd408675f710
Sajith Dilshan <sajithdilshan@gmail.com>
2014-04-21 14:38:00 +0530

    added websocket post and fixed url issue

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/github/"
-http_images_path = "/github/images"
-http_fonts_path = "/github/fonts"
-css_dir = "public/github/stylesheets"
+http_path = "blog/"
+http_images_path = "blog/images"
+http_fonts_path = "blog/fonts"
+css_dir = "public/blog/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
